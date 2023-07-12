9ac684e1534ba1fef79907a0c004a33a9aaaf85d
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-07-12 23:04:34 +0530

    login form viewed success

--- src/main/java/lk/playTech/chatApp/server/Server.java
+++ src/main/java/lk/playTech/chatApp/server/Server.java
@@ -1,4 +1,27 @@
 package lk.playTech.chatApp.server;
 
-public class Server {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Server extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login_form.fxml"))));
+        primaryStage.setTitle("Server");
+        primaryStage.centerOnScreen();
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+    }
 }
